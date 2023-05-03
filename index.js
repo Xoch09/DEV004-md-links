9426d17ff2be0d0bf49955260eeb07891824e188
Xochitl <unmananaarchivos@gmail.com>
2023-05-02 18:48:36 -0600

    Constante con 2 argumentos

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
+const fs = require("fs");
+
 const mdLinks = (path, options) => {
   //se retorna una nueva promesa
   //contiene 2 parámetros "resolve y reject" error y verdadero
   //son funciones callback
   return new Promise((resolve, reject) => { 
+//identificar si la ruta existe
 
-  });
+if(fs.existsSync(path)){
+// chec o convierte a ruta absoluta
+//comprobar si es archivo o directorio
+// si es un directorio
+}else {
+
+
+// si no existe la ruta se rechaza la promesa
+reject("la ruta no existe");
 }
+  });
+};
 
 
-module.exports = () => {
+module.exports = {
   // ...
 };
