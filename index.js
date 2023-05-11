9afb7ecba95a0813ee354c4747459aeb151f22c8
Xochitl <unmananaarchivos@gmail.com>
2023-05-10 20:23:10 -0600

    funcion mdLinks

--- index.js
+++ index.js
@@ -1,26 +1,42 @@
-const fs = require("fs");
+import fs from 'fs';
+import path from 'path';
 
-const mdLinks = (path, options) => {
-  //se retorna una nueva promesa
-  //contiene 2 parámetros "resolve y reject" error y verdadero
-  //son funciones callback
-  return new Promise((resolve, reject) => { 
-//identificar si la ruta existe
-
-if(fs.existsSync(path)){
-// chec o convierte a ruta absoluta
-//comprobar si es archivo o directorio
-// si es un directorio
-}else {
-
-
-// si no existe la ruta se rechaza la promesa
-reject("la ruta no existe");
+export function mdLinks(file, filePath) {
+  const ext = path.extname(file);
+  if (ext === '.md') {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        console.error(`Error leyendo archivo ${filePath}: ${err}`);
+        return;
+      }
+      //filtro, discrimina
+      const regex = /\[([^\]]{0,50})\]\((https?:\/\/[^\)\s#]+)/gm;
+      let match;
+      while ((match = regex.exec(data))) {
+      console.log(`Nombre:${match[1]} \n Link: ${match[2]} \n en Ruta: ${filePath}\n`);
+      }
+    });
+  }
 }
+
+
+export function printFilesInDirectory(directoryPath) {
+  fs.readdir(directoryPath, { withFileTypes: true }, (err, files) => {
+    if (err) {
+      console.error(`Error leyendo directorio${directoryPath}: ${err}`);
+      return;
+    }
+    files.forEach((file) => {
+      const filePath = path.join(directoryPath, file.name);
+      if (file.isDirectory()) {
+        printFilesInDirectory(filePath);
+      } else {
+        mdLinks(file.name, filePath);
+      }
+    });
   });
-};
+}
 
+const userPath = process.argv[2];
+printFilesInDirectory(userPath);
 
-module.exports = {
-  // ...
-};
