34635f4a902745700ef548f9e00cd53ec17d1184
Xochitl <unmananaarchivos@gmail.com>
2023-05-29 13:32:28 -0600

    Tests al 80% con correcciones

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -3,48 +3,54 @@ import { mdLinks } from '../mdLinks.js';
 
 describe('mdLinks', () => {
   it('debería devolver un array de objetos con los enlaces encontrados en un archivo md', () => {
-    const file = 'directorioprueba';
+    const file = 'directorioprueba/prueba.md';
     return mdLinks(file)
       .then((result) => {
-        expect(result).toEqual([
+        expect(result).toStrictEqual([
           {
-            nombre: 'Enlace 1',
-            link: 'https://enlace1.com',
-            ruta: file,
+            nombre: 'Markdown',
+            link: 'https://es.wikipedia.org/wiki/Markdown',
+            ruta: "C:\\Users\\Laboratoria\\xoch\\DEV004-md-links\\directorioprueba\\prueba.md",
           },
           {
-            nombre: 'Enlace 2',
-            link: 'https://enlace2.com',
-            ruta: file,
+            nombre: 'docs oficiales de `npm install` acá',
+            link: 'https://docs.npmjs.com/cli/install',
+            ruta: "C:\\Users\\Laboratoria\\xoch\\DEV004-md-links\\directorioprueba\\prueba.md",
           },
           
         ]);
       })
-      .catch((error) => {
+      //.catch((error) => {
+        //console.log("error de tests: ", error)
      //   expect(error).toBeUndefined();
-      });
+      //});
   });
 
   it('debería devolver un array de objetos con los enlaces encontrados en todos los archivos md de un directorio',() => {
     const directory = 'directorioprueba';
     return mdLinks(directory)
       .then((result) => {
-       /* expect(result).toEqual([
+       expect(result).toEqual([
           {
-            nombre: 'Enlace 1',
-            link: 'https://enlace1.com',
-            ruta: 'directorioprueba/file1.md',
+            nombre: 'Markdown',
+            link: 'https://es.wikipedia.org/wiki/Markdown', 
+            ruta: 'directorioprueba\\prueba.md',
           },
           {
-            nombre: 'Enlace 2',
-            link: 'https://enlace2.com',
-            ruta: 'directorioprueba/file2.md',
+            nombre: 'docs oficiales de `npm install` acá',
+            link: 'https://docs.npmjs.com/cli/install',
+            ruta: 'directorioprueba\\prueba.md',
           },
-        ]);*/
+          {
+            nombre: 'Promise - MDN',
+            link: 'https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Promise',
+            ruta: 'directorioprueba\\prueba2.md',
+          },
+        ]);
       })
-      .catch((error) => {
-        expect(error).toBeUndefined();
-      });
+      //.catch((error) => {
+        //expect(error).toBeUndefined();
+      //});
   });
 
   it('debería devolver un error si se le pasa un archivo o directorio inválido', () => {
