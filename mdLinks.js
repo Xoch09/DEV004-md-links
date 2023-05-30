5cdc01159a475010f500ce2f2f789d8484be59c1
Xochitl <unmananaarchivos@gmail.com>
2023-05-30 13:28:20 -0600

    leer directorio, último

--- mdLinks.js
+++ mdLinks.js
@@ -47,25 +47,35 @@ export function mdLinks(file) {
     });
   }
   
-
+//Función de flecha, con parámetro fila (ruta de archivo que se va a leer)
 const readMd = (file) => {
+    //Hacer promesa
   return new Promise((resolve, reject) => {
+    // función "readFile" del módulo fs⬅ para leer file. 💥'utf8'💥 para que los datos se coloquen como texto.
+    //devolución llamada 💦(callback)💦 se ejecuta hasta que se lee el archivo
+        //Resultado se pasa como parametro data🟢, el error se pasa como parámetro err🔴
     fs.readFile(file, 'utf8', (err, data) => {
-      if (err) {
+      //si hay algún error
+        if (err) {
         reject(`Error leyendo archivo ${file}: ${err}`);
         return;
       }
-      //discrimina, busca similitudes
+      //discrimina, busca similitudes *expresión regular*
       const regex = /\[([^\]]{0,50})\]\((https?:\/\/[^\)\s#]+)/gm;
+      //resultados coincidencias encontradas por la expresión regular.
       let match;
+      //array vacio para almacenar los que se encuentren
       const arrayUrl = [];
+      //bucle while💨 busca las coincidencias del regex y las agrega al array
       while ((match = regex.exec(data))) {
+        //agrega al final
         arrayUrl.push({
           nombre: match[1],
           link: match[2],
           ruta: file,
         });
       }
+      // si la búsqueda se resuelve se comparte el array con los links✅
       resolve(arrayUrl);
     });
   });
@@ -78,16 +88,24 @@ const readDirectory = (directory) => {
         reject(`Error leyendo directorio ${directory}: ${err}`);
         return;
       }
+      //se filtran solo los md, nuevo 👁‍🗨array mdFiles
       const mdFiles = files.filter((file) => path.extname(file) === '.md');
+      // array creado, llamado promises que contiene promesas resultantes de aplicar la función
       const promises = mdFiles.map((file) => {
+        //para que obtenga archivos completos 
         const filePath = path.join(directory, file);
         return readMd(filePath);
       });
+      //esperar que se resuelvan las promesas
       Promise.all(promises)
+      //se ejecuta cuando se resuelven
         .then((results) => {
+            //cada resultado en results es una array, se usa 💢reduce y concat💢 
+            //para crear un solo array con todos los objetos (archivos encntrados)
         const flattenedResults = results.reduce((acc, val) => acc.concat(val),[]);
           resolve(flattenedResults);
         })
+        //se activa si se rechaza
         .catch((error) => {
             //console.log("error catch: " , error)
           reject(error);
